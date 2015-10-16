4690dcf2adf62d87255673ca5ba621ba0cf4d863
bernicewygo <bernicewygo@gmail.com>
2015-10-16 14:23:12 -0400

    Portfolio

--- js/portfolio.js
+++ js/portfolio.js
@@ -2,8 +2,12 @@ $(function() {
 	$.getJSON( "http://bernicewygo.github.io/portfolio.json", function( data ) {
 	  var items = [];
 	  $.each( data, function( key, val ) { 
-		  console.log(val);
-		items.push( "<div class='col-md-4 portfolio'><p class='title'>" + val.type +"</p><img class='image' src='img/portfolio/" + val.img + "'><b>" + val.title + "</b> - " + val.description + "</div>" );
+		console.log(val);
+		var hasDescription = "</b>";
+		if (val.description != "") {
+			hasDescription = "</b> - ";
+		}
+		items.push( "<div class='col-md-4 portfolio'><p class='title'>" + val.type +"</p><img class='image' src='img/portfolio/" + val.img + "'><b>" + val.title + hasDescription + val.description + "</div>" );
 	  });
 	 
 	  var result = items.join( "" );
