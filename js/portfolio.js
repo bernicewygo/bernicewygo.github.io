ebbaffe55eb8236a1d90b37ec85660185bf0b07a
bernicewygo <bernicewygo@gmail.com>
2015-10-19 10:20:25 -0400

    Portfolio

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,5 +1,6 @@
 var index = -1; 
 var show = 0; 
+var perpage = 6; 
 
 $(function() {
 	
@@ -8,13 +9,18 @@ $(function() {
 	  $.each( data, function( key, val ) {
 		index++;  
 		var hasDescription = "</b>";
+		var link = val.img; 
 		if (val.description != "") {
 			hasDescription = "</b> - ";
 		}
-		if (key < 3) {
-			items.push( "<div class='col-sm-4 portfolio' id='item" + key + "'><p class='title'>" + val.type +"</p><img class='image' src='img/portfolio/" + val.img + "'><b>" + val.title + hasDescription + val.description + "</div>" );
+		if (val.link != "") {
+			link = val.link;
+		}
+
+		if (key < perpage) {
+			items.push( "<div class='col-sm-4 portfolio' id='item" + key + "'><p class='title'>" + val.type +"</p><a href='img/portfolio/" + link + "' data-lightbox='item" + key + "'><img class='image' src='img/portfolio/" + val.img + "'></a><b>" + val.title + hasDescription + val.description + "</div>" );
 		} else {
-			items.push( "<div class='col-sm-4 portfolio hide' id='item" + key + "'><p class='title'>" + val.type +"</p><img class='image' src='img/portfolio/" + val.img + "'><b>" + val.title + hasDescription + val.description + "</div>" );
+			items.push( "<div class='col-sm-4 portfolio hide' id='item" + key + "'><p class='title'>" + val.type +"</p><a href='img/portfolio/" + link + "' data-lightbox='item" + key + "'><img class='image' src='img/portfolio/" + val.img + "'></a><b>" + val.title + hasDescription + val.description + "</div>" );
 
 		}
 	  });
@@ -29,14 +35,14 @@ $('#minus3').addClass('hide');
 
 $('#plus3').click(function(){
 	var counter = 0; 
-	for (var i = 0; i < 3; i++) {
+	for (var i = 0; i < perpage; i++) {
 		var name = '#item' + (show + i);
 		remove(name);  
 	}
-	show += 3; 
+	show += perpage; 
 	while ( (index - show) >= 0) {
 		var name = '#item' + show; 
-		if (counter > 3) {
+		if (counter > perpage) {
 			break; 
 		}
 		add(name); 
@@ -52,21 +58,20 @@ $('#plus3').click(function(){
 
 $('#minus3').click(function(){
 	var counter = 0; 
-	for (var i = 0; i < 3; i++) {
+	for (var i = 0; i < perpage; i++) {
 		var name = '#item' + (show - i);
 		remove(name);  
 	}
-	show -= 2; 
+	show -= perpage - 1; 
+	console.log(show); 
 	while ( show >= 0) {
-		var name = '#item' + show; 
-		if (counter > 3) {
+		var name = '#item' + (show + counter); 
+		if (counter > perpage) {
 			break; 
 		}
 		add(name); 
 		counter++;
-		show--; 
 	}
-	show++; 
 	if ($('#plus3').hasClass('hide'))
 		add('#plus3');
 	if (0 == show)
