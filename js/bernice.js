394b9cf957ccafa9bcafe82fc44057038504f8c2
bernicewygo <bernicewygo@gmail.com>
2015-11-27 11:08:40 -0500

    modal work

--- js/bernice.js
+++ js/bernice.js
@@ -5,11 +5,15 @@
 
 	var app = angular.module('portfolio', []); 
 
-	app.controller('portfolioController', function($scope, $http) {
+	app.controller('portfolioController', function($scope, $http, $sce) {
 	  $http.get('http://bernicewygo.github.io/portfolio.json')
 	       .then(function(res){
 	          $scope.infos = res.data;                
 	        });
+
+		$scope.getHTMLvalue = function(html) {
+          return $sce.trustAsHtml(html);	
+         };       
 	});
 
 	app.controller("panelController", function() {
