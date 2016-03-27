62a4473b5a1963a00178b624e23bcf818176b13b
bernicewygo <bernicewygo@gmail.com>
2016-03-27 17:09:22 -0400

    added dialogs for projects

--- js/app.js
+++ js/app.js
@@ -81,9 +81,45 @@ app.controller('MainController',
 		hideSection('#gallery', '.portfolio'); 
 		viewSection('#credentials', '.cred'); 
 	}
+
+	$scope.showModal = function(ev) {
+    var useFullScreen = ($mdMedia('sm') || $mdMedia('xs'))  && $scope.customFullscreen;
+    $mdDialog.show({
+      controller: DialogController,
+      templateUrl: 'projects/' + ev + '.tmpl.html',
+      parent: angular.element(document.body),
+      targetEvent: ev,
+      clickOutsideToClose:true,
+      fullscreen: useFullScreen
+    })
+
+    .then(function(answer) {
+      $scope.status = 'You said the information was "' + answer + '".';
+    }, function() {
+      $scope.status = 'You cancelled the dialog.';
+    });
+    $scope.$watch(function() {
+      return $mdMedia('xs') || $mdMedia('sm');
+    }, function(wantsFullScreen) {
+      $scope.customFullscreen = (wantsFullScreen === true);
+    });
+
+
+  };
    
 }]);
 
+function DialogController($scope, $mdDialog) {
+  $scope.hide = function() {
+    $mdDialog.hide();
+  };
+  $scope.cancel = function() {
+    $mdDialog.cancel();
+  };
+  $scope.answer = function(answer) {
+    $mdDialog.hide(answer);
+  };
+}
 
 $(function() {
 				$('#projects').hide();
